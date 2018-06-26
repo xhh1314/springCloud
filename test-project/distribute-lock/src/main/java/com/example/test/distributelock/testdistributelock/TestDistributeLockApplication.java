44099cdcd7f95367762d731e69554ba68afd81b9
雕兄 <903440799@qq.com>
2018-06-26 21:53:28 +0800

    d

---
+++ test-project/distribute-lock/src/main/java/com/example/test/distributelock/testdistributelock/TestDistributeLockApplication.java
@@ -0,0 +1,12 @@
+package com.example.test.distributelock.testdistributelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDistributeLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDistributeLockApplication.class, args);
+	}
+}
---
+++ test-project/spring-aop-test/src/main/java/com/example/test/aop/springaoptest/SpringAopTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.test.aop.springaoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopTestApplication.class, args);
+	}
+}
